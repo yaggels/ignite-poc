e9cc934acb0e66c44872b9710a53005e4b4345a4
jchow2 <jchow2@DCjchow268224.sapient.com>
2017-10-05 08:53:48 -0400

    feat: change source of statistics to JPA entity manager.
    TODO: still not working but no negative impact.

--- src/main/java/com/sapient/ignite/poc/monitors/CacheMonitor.java
+++ src/main/java/com/sapient/ignite/poc/monitors/CacheMonitor.java
@@ -3,27 +3,37 @@ package com.sapient.ignite.poc.monitors;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernateEntityManager;
 import org.hibernate.stat.Statistics;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.provider.HibernateUtils;
 
+@Aspect
 public class CacheMonitor {
 	private final static Logger LOG = LogManager.getLogger(CacheMonitor.class);
 	private final static NumberFormat NF = new DecimalFormat("0.0###");
 
+	//@Autowired
+	//private SessionFactory sessionFactory;
 	@Autowired
-	private SessionFactory sessionFactory;
+	private EntityManagerFactory entityManagerFactory;
 	
-	@Around("execution(* com.sapient.ignite.poc.beans..*.*(..))")
+	@Around("execution(* com.sapient.ignite.poc.beans.User(..))")
 	public Object log(ProceedingJoinPoint pjp) throws Throwable {
 		if (!LOG.isDebugEnabled()) {
 			return pjp.proceed();
 		}
 		
+		
+		SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
 		Statistics statistics = sessionFactory.getStatistics();
 		statistics.setStatisticsEnabled(true);
 
